cd9f831f030e435e39fc30071b973baa83ffd515
Lumynous <lumynou5.tw@gmail.com>
2025-05-08 14:41:57 +0800

    Use script to bump version

---
+++ bump_version.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+sed '
+/## \[Unreleased\]/a\
+\n## ['"$1"'] - '"$(date -I)"'
+1,/^\[[0-9]\+\.[0-9]\+\.[0-9]\+\]/{
+//i\
+['"$1"']: https://github.com/lumynou5/live-time-copier/releases/tag/v'"$1"'
+}
+' CHANGELOG.md >dist/CHANGELOG.md
+mv dist/CHANGELOG.md CHANGELOG.md
+
+sed '
+s/"version": "[0-9]\+\.[0-9]\+\.[0-9]\+"/"version": "'"$1"'"/
+' src/manifest.json >dist/manifest.json
+mv dist/manifest.json manifest.json
